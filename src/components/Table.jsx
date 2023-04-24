10f20e7ce8a47c25c12e025580efd568bf20e793
SULAJSJS <chistotiktok@gmail.com>
2023-04-24 13:14:39 +0600

    fixes

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,25 +3,6 @@ import React, { useRef, useState } from 'react';
 const Table = ({ months, store, market, setIsMarket }) => {
   const inputRef = useRef();
   const storeEl = store;
-  const sum = () => {
-    const initialValue = 0;
-    return market.map((item) =>
-      console.log(
-        item.months.reduce(
-          (accumulator, currentValue) => console.log('data', accumulator + currentValue),
-          initialValue,
-        ),
-      ),
-    );
-  };
-  //   const handleChange = (el, e) => {
-  //     data.map((item) =>
-  //       item.months.map((elem) => {
-  //         return el.id === elem.id ? { ...elem, value: Number(e.target.value) } : elem;
-  //       }),
-  //     );
-  //     sum();
-  //   };
 
   const handleChange = (monthId, value) => {
     const store = market.find((el) => el.store.id === storeEl.id);
