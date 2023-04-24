30505eff7d0e5e928fbc941c7e3cdd40b0f59a86
SULAJSJS <chistotiktok@gmail.com>
2023-04-24 13:12:01 +0600

    fix

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const Table = ({ months, store, market, setIsMarket }) => {
+  const inputRef = useRef();
   const storeEl = store;
   const sum = () => {
     const initialValue = 0;
@@ -27,7 +28,7 @@ const Table = ({ months, store, market, setIsMarket }) => {
 
     const needMonth = store?.months?.filter((el) => {
       if (el.id === monthId) {
-        el.value = Number(value);
+        el.value = Number(value.substr(0, 7));
       }
     });
 
@@ -53,10 +54,15 @@ const Table = ({ months, store, market, setIsMarket }) => {
       <div className="values">
         {months.map((el, i) => (
           <input
+            ref={inputRef}
             onChange={(e) => handleChange(el.id, e.target.value)}
             className="input"
-            type="text"
-            value={el.value}
+            type="number"
+            value={
+              Number(el.value.toString().length) <= 7
+                ? el.value
+                : Number(el.value.toString().substr(0, 7))
+            }
           />
         ))}
       </div>
